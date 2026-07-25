b900a0ab75e3d4870133ea208b7aceeff1363888
riteshjillewad <riteshjillewad292@gmail.com>
2026-07-25 19:35:11 +0530

    add sum of even numbers logic

---
+++ Assignment-23/Assignment_23_1.py
@@ -0,0 +1,73 @@
+##########################################################################################
+# Name:        Assignment_23_1.py
+# Description: Sum of all even numbers from 1 to N(each list number)
+# Input:       list
+# Output:      Sum of even numbers from 
+# Date:        25-07-2026
+# Author:      Ritesh Jillewad
+##########################################################################################
+
+import multiprocessing
+import time
+import os
+
+def work(N):
+    
+    sumEven = 0
+    
+    for i in range(2, N + 1):
+        if i % 2 == 0:
+            sumEven += i
+            
+    pid = os.getpid()
+    
+    print(f"Process ID: {pid}")
+    print(f"Input Number: {N}")
+    print(f"Sum of even numbers: {sumEven}")
+            
+    return sumEven
+
+        
+          
+def main():
+    
+    ##########################################################################
+    # INPUT LIST PART
+    ##########################################################################
+    
+    input_list = list()
+    
+    print("Enter size of list: ")
+    size = int(input())
+    
+    print(f"Enter {size} elements: ")
+    for _ in range(size):
+        value = int(input())
+        input_list.append(value)
+        
+    ##########################################################################
+    # INPUT LIST VALIDATION PART
+    ##########################################################################
+        
+    if len(input_list) == 0:
+        print("Input list empty!")
+        return
+    else:
+        print(f"Input list: {input_list}")
+        
+    ##########################################################################
+    # MULTIPROCESSING PART
+    ##########################################################################
+    
+    start_time = time.perf_counter()
+    
+    with multiprocessing.Pool() as pool:
+        result = pool.map(work, input_list)
+        
+    end_time = time.perf_counter()
+        
+    print(f"Result: {result}")
+    print(f"Time taken: {end_time - start_time:.6f} seconds")
+        
+if __name__ == "__main__":
+    main()
